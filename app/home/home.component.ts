1af47c79dced0b22b5a42fbee40512dab0558137
lisabell-li <lisabell.li@gmail.com>
2018-06-07 15:52:41 +0200

    add bucket list items functionality

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = "add an item";
+  goalText: string = "My first goal";
+  goals =[];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    if(this.goalText){
+    this.goals.push(this.goalText);
+    this.goalText ="";
+    this.itemCount = this.goals.length;
+    }
+    else return;
   }
 
 }
